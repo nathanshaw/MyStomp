f315f8ce9ae8e3420c02915334c47e68f1070751
Nathan Shaw <nathanshawsemail@gmail.com>
2015-02-27 22:37:01 -0800

    Started Master Code
    With startup modes

---
+++ firmware/master_0_1/master_0_1.ino
@@ -0,0 +1,93 @@
+/*
+  My-Stomp Master Program
+ 
+ Programed by Nathan Villicana-Shaw
+ Spring 2015
+ CalArts MTIID
+ 
+ */
+
+//to time shiz
+unsigned long last;
+unsigned long now;
+int bootTime = 1000;
+
+int mode = 0;
+int record = 0;
+int metro = 0;
+/*
+  0 = nothing pressed                    (pedal)
+ 1 = 2end button pressed                 (synth)
+ */
+
+const int butNum = 3;
+
+const int button[butNum] = {
+  8,9,10  
+};
+int oldButState[butNum] = {
+  0,0,0 
+};
+int butState[butNum] = {
+  0,0,0 
+};
+
+//loop this for a second to check for input, dont include else 
+void setMode(){
+  //  last = millis();
+  //while(now > last + bootTime){
+  for(int i = 0; i < butNum; i++){
+    butState[i] = digitalRead(button[i]);
+  }
+  //mode is initalized as 0 by default
+  if(button[0] == 1){
+    mode = 1; 
+    Serial.println("Enable Synth Mode");
+  }
+  //check for recording
+  if(button[1] == 1){
+    record = 1; 
+    Serial.println("Enable Recording");
+  }
+  //check for metronome
+  if(button[2] == 1){
+    metro = 1;
+    Serial.println("Enable Metronome");
+  }
+  //check to see if metronome should be initalized
+  if(button[2] == 1){
+
+    //send message to initalize metronome in chuck 
+  }
+  //now == millis();
+  //}
+  if(metro == 1){
+    Serial.println("Metronome Start Message Sent to Chuck");
+  }
+  if(record == 1){
+    Serial.println("Recording Start Message Sent to Chuck");    
+  }
+  if(mode == 1){
+    Serial.println("Starting Chuck in Synth Mode");
+  }
+}
+
+void setup(){
+
+  Serial.begin(57600);
+  //just for testing with buttons
+  for(int i = 0; i < butNum; i++){
+    pinMode(button[i], INPUT);
+  }
+  //some time to even out
+  delay(100);
+  //setup modes
+  setMode();   
+}
+
+void loop(){
+ 
+ Serial.println("Main Loop");
+ delay(1000);
+}
+
