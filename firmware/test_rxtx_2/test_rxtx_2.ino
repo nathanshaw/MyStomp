7a06e6287ff92a519c6e9fa311f0ba7a6974647c
Nathan Shaw <nathanshawsemail@gmail.com>
2015-03-29 03:03:15 -0700

    Added test code for arduino

---
+++ firmware/test_rxtx_2/test_rxtx_2.ino
@@ -0,0 +1,13 @@
+
+//test sketch to test my-stomp
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Hello Pi");
+  delay(500);
+}
